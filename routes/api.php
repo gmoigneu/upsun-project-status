4442b6c3976e306b0d67c9688844c7001ec07a20
Guillaume Moigneu <g@moigneu.com>
2024-04-12 16:26:15 -0500

    Redirect message on the API

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
 use App\Http\Controllers\Api\StatusController;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return new JsonResponse([
+        'message' => 'Welcome to the API. Please check https://github.com/gmoigneu/upsun-project-status.',
+    ]);
+});
 
 Route::get('/{project:project}/{environment:name}', [StatusController::class, 'show']);
 Route::post('/', [StatusController::class, 'update']);
