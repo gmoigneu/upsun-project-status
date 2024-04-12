d033cf1a8e90ea7866b0d9eeec61d4560e57cb9a
Guillaume Moigneu <g@moigneu.com>
2024-04-12 14:48:16 -0500

    Return status from cache or db

--- app/Http/Controllers/Api/StatusController.php
+++ app/Http/Controllers/Api/StatusController.php
@@ -8,21 +8,30 @@ use App\Models\Activity;
 use App\Models\Environment;
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class StatusController extends Controller
 {
     public function show(Request $request, Project $project, Environment $environment)
     {
+        // Fetch the status from the cache
+        $available = Cache::get('env-' . $environment->id);
+
+        // If not in cache, check the database and set it
+        if (!$available) {
+            $available = $environment->is_available;
+            Cache::put('env-' . $environment->id, $available);
+        }
+
         return response()->json([
             'project' => $project->project,
             'environment' => $environment->name,
-            'state' => true,
+            'available' => $available,
         ]);
     }
 
     public function update(NewActivityRequest $request)
     {
-
         $project = Project::where('project', $request->project)->firstOrCreate([
             'project' => $request->project,
         ]);
